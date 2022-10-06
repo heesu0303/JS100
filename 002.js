0dec322419ec539cde3a55b85d202b0a2b5c33b6
Heesu <heesu0303@naver.com>
2022-10-06 20:31:45 +0900

    JS100: 1-5

---
+++ 002.js
@@ -0,0 +1,8 @@
+// 002. 배열의 내장함수
+// <pass>부분에 배열 내장함수를 이용하여 코드를 입력하고 다음과 같이 출력되게 하세요.
+
+let arr = [200, 100, 300];
+//pass
+//arr.splice(2, 0, 10000);
+let spread = [...arr.slice(0,2), 10000, ...arr.slice(2)];
+console.log(spread);
---
+++ 003.js
@@ -0,0 +1,7 @@
+// 003. 변수의 타입
+// 다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof(arr)); 
+// output: object
+// 변수의 타입 : 원시타입과 객체(object)로 나뉨
